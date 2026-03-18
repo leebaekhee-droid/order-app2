dc1fcf51be8bf80a26199544e3b1adbf82c56c8a
rusbhlee <leebaekhee@gmail.com>
2026-03-18 20:31:32 +0500

    Fix Render backend start path

---
+++ server/index.js
@@ -0,0 +1,3 @@
+// Render에서 start command를 `node index.js`로 설정했을 때 진입점 역할
+require('./src/index.js')
+
---
+++ src/server/index.js
@@ -0,0 +1,3 @@
+// Render 배포 설정에 따라 start 경로가 `src/server/index.js`를 기대하는 경우 대응
+require('../../server/src/index.js')
+
