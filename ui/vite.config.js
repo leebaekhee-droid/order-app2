5f697ec8d5983fb1474c2533d1b6dd285f61302f
rusbhlee <leebaekhee@gmail.com>
2026-03-19 13:58:22 +0500

    Update production environment configuration and Vite preview settings

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['order-app2-backend-2bnl.onrender.com'],
+  },
 })
